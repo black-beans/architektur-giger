3b65a3f4a3955f82fae897b0ccd7163b0c12a8eb
Michael Kessler <michi@flinkfinger.com>
2014-02-26 20:10:37 +0100

    Initial slideshow with content.

--- source/scripts/application.js
+++ source/scripts/application.js
@@ -1,6 +1,8 @@
 /*
  *= require './vendor/modernizr-2.6.2.min'
  *= require './vendor/jquery-1.10.2.min'
+ *= require './vendor/jquery.colorbox-min'
+ *= require './vendor/jquery.colorbox-de'
  *= require_self
  */
 
